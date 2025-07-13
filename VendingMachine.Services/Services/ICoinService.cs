f518303eff1f91b5f76bb5c375012da2ce036e91
yana.radeva7 <yana.radeva7@gmail.com>
2025-07-13 22:16:52 +0300

    add empty lines for readability

--- VendingMachine.Services/Services/ICoinService.cs
+++ VendingMachine.Services/Services/ICoinService.cs
@@ -5,8 +5,12 @@ namespace VendingMachine.Services.Services;
 public interface ICoinService
 {
     Task DepositAsync(Dictionary<byte, int> coins);
+
     Task DepositAsync(CoinDto coin);
+
     Task DecreaseInventoryAsync(Dictionary<byte, int> coins);
+
     Task DecreaseInventoryAsync(CoinDto coin);
+
     Task<IEnumerable<CoinDto>> GetAllDescendingAsNoTrackingAsync();
 }
--- VendingMachine.Services/Services/IProductService.cs
+++ VendingMachine.Services/Services/IProductService.cs
@@ -6,12 +6,20 @@ namespace VendingMachine.Services.Services;
 public interface IProductService
 {
     Task<Product> GetByCodeAsync(string code);
+
     Task<IEnumerable<ProductDto>> GetAllAsNoTrackingAsync();
+
     Task<IEnumerable<string>> GetAllCodesAsync();
+
     Task DecreaseInventory(string code);
+
     Task UpdateQuantityAsync(ProductQuantityUpdateDto product);
+
     Task UpdatePriceAsync(ProductPriceUpdateDto product);
+
     Task RemoveAsync(string code);
+
     Task AddAsync(ProductDto product);
+
     Task<bool> CanAddAsync();
 }
